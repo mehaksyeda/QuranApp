033813076ca07dd021bd0a2e81a9f022775689d5
mehaksyeda <mehaksyeda78@gmail.com>
2022-07-28 15:05:27 +0500

    stylesheet

---
+++ style/styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    container: {
+        
+        backgroundColor:"white",
+        height:"100%"
+
+      
+     
+   
+      
+    },
+    container1:{
+        backgroundColor:"white",
+        borderBottomColor:"#105278",
+        borderBottomWidth:1,
+        padding:8
+
+    },
+    title: {
+        alignSelf:"center",
+        
+      color: "#fdce51",
+      fontSize: 25,
+      fontWeight: "bold"
+    },
+    englishName:{
+        color:"#105278", fontSize:24
+    },
+    Name:{
+        fontSize:24,color:"#fdce51"
+
+    },
+    image:{
+        height:"100%",
+        width:"30%",
+        alignSelf:"flex-start",
+        color:"white"
+
+    }
+  });
+  export default styles;
